1421bae4df433e36dae15eb7a01ee90e648adbf6
seyam <mdseyam3215@gmail.com>
2023-10-20 20:20:48 +0600

    registration validation

--- src/Component/Login/Register/Register.jsx
+++ src/Component/Login/Register/Register.jsx
@@ -1,89 +1,164 @@
-import { useContext } from "react";
-import { AuthContext } from "../../../Provider/AuthProvider";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Provider/AuthProvider";
 import Swal from "sweetalert2";
+
 
 
 const Register = () => {
 
-    const { createUser } = useContext(AuthContext);
-    
-    const handleRegister = e => {
-        e.preventDefault();
-        const form = e.target;
-        const displayName = form.displayName.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(displayName,email, password);
+    const [registerError, setRegisterError] = useState('');
+  const { createUser } = useContext(AuthContext);
 
-        createUser(email, password)
-            .then(result => {
-                console.log(result.user);
-                // new user has been created
-                const createdAt = result.user?.metadata?.creationTime;
-                const user = {displayName, email, createdAt: createdAt };
-                fetch('http://localhost:5000/user', {
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'application/json'
-                    },
-                    body: JSON.stringify(user)
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        if(data.insertedId){
-                            Swal.fire(
-                                'Register success'
-                              )
-                        }
-                    })
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    
+  const handleRegister = e => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const name = form.get("name");
+    const email = form.get("email");
+    const password = form.get("password");
+
+    // Check if the password meets your criteria
+    const message = checkPasswordValidity(password);
+
+    if (message) {
+      setRegisterError(message);
+      return;
     }
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col ">
-            <div className="text-center ">
-                <h1 className="text-5xl font-bold">Register</h1>
+    // Create the user in Firebase Authentication
+    createUser( email, password)
+      .then(result => {
+        // Registration was successful
+        console.log(result.user);
+        const createdAt = result.user?.metadata?.creationTime;
+        const user = {name, email, createdAt: createdAt };
 
-            </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            
-                <form  onSubmit={handleRegister}  className="card-body">
-                <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">DisplayName</span>
-                        </label>
-                        <input type="text" name="displayName" placeholder="displayName" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Register</button>
-                    </div>
-                </form>
-            <Link className="text-center mb-6" to={"/login"}>
-                <button className="btn btn btn-accent">Login</button>
-            </Link>
-            </div>
-        </div>
+        // Send user data to your server (Assuming you have a server)
+        fetch('http://localhost:5000/user', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+          .then(res => res.json())
+          .then(data => {
+            if (data.insertedId) {
+              Swal.fire("Registration Successful!", "You are now registered.", "success");
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      })
+      .catch(error => {
+        // Registration failed
+        if (error.code === 'auth/email-already-in-use') {
+          setRegisterError('Email is already in use. Please choose a different one.');
+        } else {
+          setRegisterError('Registration error. Please try again.');
+        }
+      });
+  };
+
+  // Password validation function
+  const checkPasswordValidity = (password) => {
+    // Define your password validation criteria here
+    const isNonWhiteSpace = /^\S*$/;
+    if (!isNonWhiteSpace.test(password)) {
+      return "Password must not contain Whitespaces.";
+    }
+
+    const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+    if (!isContainsUppercase.test(password)) {
+      return "Password must have at least one Uppercase Character.";
+    }
+
+    const isContainsLowercase = /^(?=.*[a-z]).*$/;
+    if (!isContainsLowercase.test(password)) {
+      return "Password must have at least one Lowercase Character.";
+    }
+
+    const isContainsNumber = /^(?=.*[0-9]).*$/;
+    if (!isContainsNumber.test(password)) {
+      return "Password must contain at least one Digit.";
+    }
+
+    const isContainsSymbol =
+      /^(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*$/;
+    if (!isContainsSymbol.test(password)) {
+      return "Password must contain at least one Special Symbol.";
+    }
+
+    const isValidLength = /^.{8,16}$/;
+    if (!isValidLength.test(password)) {
+      return "Password must be 8-16 Characters Long.";
+    }
+
+    return null; // Password is valid
+  };
+
+  return (
+    <div className="mb-16">
+      <h2 className="text-2xl text-center mt-5">Please Register</h2>
+      <div className="card flex-shrink-0 mx-auto mt-6 w-full max-w-sm shadow-2xl bg-base-100">
+        <form onSubmit={handleRegister} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+        <p className="mb-5 ml-8">
+          Already have an account? Please{" "}
+          <Link to="/login">
+            <span className="text-blue-700 font-bold">Login</span>
+          </Link>
+        </p>
+      </div>
+      <div className="text-center mt-3 text-red-600">
+        {registerError && <p>{registerError}</p>}
+      </div>
     </div>
-);
+  );
 };
-
-
 export default Register;
