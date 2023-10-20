0b683900f6ec8ab040d900d86434b6a146d6994c
seyam <mdseyam3215@gmail.com>
2023-10-21 03:51:34 +0600

    responsive for mb device 1

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
           
     </>
   return (
-    <div className="navbar bg-base-200 px-10">
+    <div className="navbar bg-base-200 md:px-10 ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
